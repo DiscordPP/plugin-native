9764176651c545e792819a5426b3d8f18513de0f
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2022-02-21 20:02:04 -0600

    Added scheduled event objects

---
+++ discordpp/objects_fwd/guild-scheduled-event_fwd.hh
@@ -0,0 +1,12 @@
+//
+// Created by Aidan on 4/20/2021.
+//
+
+
+#ifndef OBJECT_BREAKOUTS
+#error This header should only be included in plugin-native.hh
+#endif
+
+class GuildScheduledEvent;
+class GuildScheduledEventMetadata;
+class GuildScheduledEventUser;
